3a743ea40232051dd93083b97fcd6e05421bd3f1
Zehra Ecer <zehraecer1996@hotmail.com>
2024-06-28 00:04:56 +0300

    hata çözüldü

--- src/pages/productDetail.jsx
+++ src/pages/productDetail.jsx
@@ -17,7 +17,42 @@ export const ProductDetail = () => {
 
     return (
         <>
+            <div className="product">
 
+                <div className="product-left">
+                    <img src={`${post.images}`} alt="" />
+
+                </div>
+
+                <div className="product-right">
+
+                    <h4>Sneaker Company</h4>
+                    <h3>Fall Limited Edition Sneakers</h3>
+                    <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+
+                    <div className="price">
+                        <div className="price-left">
+                            <h5>$125.00</h5>
+                            <h6>$250.00</h6>
+                        </div>
+                        <div>
+
+                            <button>50%</button>
+                        </div>
+                    </div>
+
+                    <div className="product-lower">
+
+                        <div className="stock">
+                            <button>-</button>
+                            <h4>0</h4>
+                            <button>+</button>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
